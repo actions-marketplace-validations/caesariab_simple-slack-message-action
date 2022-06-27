c0ef09630a0e98ece4a15a7e29aee551de9d5bfb
Thim Lohse <thim.lohse@caesari.se>
2022-06-27 14:35:24 +0200

    updated script with output and error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh -l
 DATA=$1
 HOOK_SLACK_URL=$2
-curl -X POST -H 'Content-type: application/json' --data "{'text': '$DATA'}" $HOOK_SLACK_URL
+
+send_message() {
+    curl -X POST -H 'Content-type: application/json' --data "{'text': '$DATA'}" $HOOK_SLACK_URL && echo "::set-output name=success::true"
+
+}
+handle_error() {
+    echo "Error: $1"
+    echo "::set-output name=success::false"
+    exit 1
+}
+
+send_message || handle_error "Sending message failed."
