5b027e0591e874592b8d84c5f2e099d0a32317fe
Thim Lohse <thim.lohse@caesari.se>
2022-06-30 10:52:22 +0200

    updated workflow to silence output of curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ DATA=$1
 HOOK_SLACK_URL=$2
 
 send_message() {
-    curl -X POST -H 'Content-type: application/json' --data "{'text': '$DATA'}" $HOOK_SLACK_URL && echo "::set-output name=success_status::true"
+    curl -X POST -H 'Content-type: application/json' --data "{'text': '$DATA'}" $HOOK_SLACK_URL 1>/dev/null &&
+        echo "::set-output name=success_status::true"
 
 }
 
@@ -13,5 +14,6 @@ handle_error() {
     exit 1
 }
 
-echo "::set-output name=action_fruit::strawberry"
+echo "::set-output name=action_fruit::blueberry"
 send_message || handle_error "Sending message failed."
+# echo "::set-output name=success_status::true"
